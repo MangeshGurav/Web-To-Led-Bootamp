100ea5d869b228e34d59c629bf74e3d698b89e13
MangeshGurav <88697444+MangeshGurav@users.noreply.github.com>
2024-08-11 10:07:52 +0530

    reCAPTCHA validations

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
+let captchaChecked = false;
 
 function beforeSubmit(event) {
-    let inputDate = document.querySelector('.inputDate');
-    let outputDate = document.querySelector('.outputDate');
+  if (captchaChecked) {
+    let inputDate = document.querySelector(".inputDate");
+    let outputDate = document.querySelector(".outputDate");
 
     console.log("inputDate==>", inputDate.value); // string --> date  --> local fornamt (en_IN)
 
@@ -11,19 +12,25 @@ function beforeSubmit(event) {
 
     outputDate = formattedDate;
     console.log("outputDate==>", outputDate);
-
+  }else{
+    alert("Please check reCAPTCHA box to submit the lead.")
+    event.preventDefault();
+  }
 }
 
-function timestamp() { 
-    var response = document.getElementById("g-recaptcha-response"); 
-    if (response == null || response.value.trim() == "") { 
-        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value); 
-        elems["ts"] = JSON.stringify(new Date().getTime()); 
-        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
-    } 
-} setInterval(timestamp, 500);
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
 
-
-
-
-
+function capthcsucess() {
+  captchaChecked = true;
+}
