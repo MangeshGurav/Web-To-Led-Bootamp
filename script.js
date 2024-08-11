285523b377fc10141f7f66a0cc3098c8276f4493
MangeshGurav <88697444+MangeshGurav@users.noreply.github.com>
2024-08-11 09:56:40 +0530

    Final Changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-function beforeSubmit(event){
+function beforeSubmit(event) {
     let inputDate = document.querySelector('.inputDate');
     let outputDate = document.querySelector('.outputDate');
 
@@ -13,6 +13,16 @@ function beforeSubmit(event){
     console.log("outputDate==>", outputDate);
 
 }
+
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") { 
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value); 
+        elems["ts"] = JSON.stringify(new Date().getTime()); 
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    } 
+} setInterval(timestamp, 500);
+
 
 
 
